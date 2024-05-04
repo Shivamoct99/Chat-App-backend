7dce75d87d3d6185fc009f7c1e1dc02d0ac52dd3
shivam sharma <shivamsharma11111111@gmail.com>
2024-05-04 17:42:16 +0530

    socket io server experiment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
-const server = require("http").createServer(app);
+const server = require("https").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: process.env.CLIENT_URL || "http://localhost:3000",
